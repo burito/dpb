89aa089fd22b58f016bbce9af8de8e170988a59d
Daniel Burke <dan.p.burke@gmail.com>
2019-02-01 12:08:38 +1030

    Added a sys_dpi test to the gl test

--- test/gfx_gl.c
+++ test/gfx_gl.c
@@ -21,30 +21,38 @@ int main_init(int argc, char *argv[])
 
 void main_loop(void)
 {
+	// test that the keyboard works
 	if(keys[KEY_ESCAPE])
 	{
 		log_info("Shutdown on : Button press (ESC)");
 		killme=1;
 	}
-	
+
+	// clear the window to green	
 	glClearColor(0,1,0,1);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	// draw a blue box in the center of the window
 	glColor4f(0,0,1,1);
 	glRectf( -0.5, -0.5, 0.5, 0.5);
 
+	// draw a 25 pixel tall box, that scales with the system UI elements
+	float y = ( 25.0 / ((float)vid_height / 2.0) ) * sys_dpi;
+	glRectf( -0.8, -0.8, 0.8, -0.8+y);
+
+	// Figure out the OpenGL screen coordinates of the mouse
 	float mx = (float)mouse_x / (float)vid_width;
 	float my = (float)mouse_y / (float)vid_height;
 	mx = mx * 2 - 1;
 	my = my * 2 - 1;
 	my = -my;
 	float md = 0.01;
-
+	// draw a red box around the mouse
 	glColor4f(1,0,0,1);
 	glRectf( mx-md, my-md, mx+md, my+md);
 
-//	log_info("win = (%d, %d) mouse = (%d, %d)", vid_width, vid_height, mouse_x, mouse_y);
-//	log_info("m = (%f, %f)", mx, my);
+	
 
+	// swap the buffer onto the screen
 	gfx_swap();
 }
 
