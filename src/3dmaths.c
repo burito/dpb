44f09265cda113b02bea6984fccef4bff3bf90e9
Daniel Burke <dan.p.burke@gmail.com>
2021-03-15 14:22:03 +1030

    typo in mat4x4_print

--- src/3dmaths.c
+++ src/3dmaths.c
@@ -52,7 +52,7 @@ float finvsqrt(float x)
 void mat4x4_print(mat4x4 m)
 {
 	printf("\t%f\t%f\t%f\t%f\n\t%f\t%f\t%f\t%f\n\t%f\t%f\t%f\t%f\n\t%f\t%f\t%f\t%f\n",
-		m.m[0][0], m.m[1][1], m.m[0][2], m.m[0][3],
+		m.m[0][0], m.m[0][1], m.m[0][2], m.m[0][3],
 		m.m[1][0], m.m[1][1], m.m[1][2], m.m[1][3],
 		m.m[2][0], m.m[2][1], m.m[2][2], m.m[2][3],
 		m.m[3][0], m.m[3][1], m.m[3][2], m.m[3][3]
